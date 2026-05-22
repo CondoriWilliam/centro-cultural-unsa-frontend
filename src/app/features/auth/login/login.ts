e5473f9c415e4c646fd6e693c66cbca9cecd6e55
William Condori <91301987+CondoriWilliam@users.noreply.github.com>
2026-05-22 16:38:09 -0500

    feature(public): create public layout

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LucideApple, LucideEyeClosed, LucideEyeOff, LucideUser, LucideX } from '@lucide/angular';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [LucideX,LucideEyeOff, LucideUser],
+  imports: [LucideX, LucideEyeOff, LucideUser, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LucideApple, LucideEyeClosed, LucideEyeOff, LucideMail, LucideUser, LucideX } from '@lucide/angular';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [LucideX,LucideEyeOff, LucideUser, LucideMail],
+  imports: [LucideX, LucideEyeOff, LucideUser, LucideMail, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
