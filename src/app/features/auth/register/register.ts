693febce7070d3b9edbe137f12090e366c750820
William Condori <91301987+CondoriWilliam@users.noreply.github.com>
2026-05-22 16:08:23 -0500

    feature: create register layout

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LucideApple, LucideEyeClosed, LucideEyeOff, LucideMail, LucideUser, LucideX } from '@lucide/angular';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [LucideX,LucideEyeOff, LucideUser, LucideMail],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
+
 export class RegisterComponent {}
