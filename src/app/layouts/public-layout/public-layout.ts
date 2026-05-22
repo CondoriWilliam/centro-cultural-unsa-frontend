c6bdfa4bb2edadfb1d06000505e105d4b1a3fcd4
William Condori <91301987+CondoriWilliam@users.noreply.github.com>
2026-05-22 16:09:33 -0500

    feature: create public layout

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar';
 import { FooterComponent } from '../../shared/components/footer/footer';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-public-layout',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.css',
 })
